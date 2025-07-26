7cc709d38ca5ce404f4f2df9e880b15f5bb82781
Hoang Tran <hoang.tran@apps-cyclone.com>
2025-07-26 11:15:48 +0700

    feat(route-not-match): log error

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,10 @@
+const AppError = require("../controllers/ErrorController");
 const authRouter = require("./auth");
 
 const route = (app) => {
   app.use("/v1/auth", authRouter);
+  app.all(/(.*)/, (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+  });
 };
 module.exports = route;
