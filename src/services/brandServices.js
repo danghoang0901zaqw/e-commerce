7cc5f1d28911be2d283f0f5eed328729ce5e1943
Hoang Tran <hoang.tran@apps-cyclone.com>
2025-07-27 12:05:29 +0700

    feat(count-product): handle count total product in category and brand

--- src/services/brandServices.js
+++ src/services/brandServices.js
@@ -2,9 +2,23 @@ const models = require("../models");
 
 class BrandServices {
   async getAll() {
-    const brands = await models.Brand.findAll({ raw: true });
-    console.log(brands);
-    return brands;
+    const brands = await models.Brand.findAll({
+      include: [
+        {
+          model: models.Product,
+          as: "products",
+        },
+      ],
+    });
+    const mappingBrands = brands.map((brand) => {
+      const brandData = brand.get({ plain: true });
+      const { products, ...rest } = brandData;
+      return {
+        ...rest,
+        totalProduct: products.length,
+      };
+    });
+    return mappingBrands;
   }
 }
 module.exports = new BrandServices();
--- src/services/categoryServices.js
+++ src/services/categoryServices.js
@@ -1,9 +1,23 @@
 const models = require("../models");
-
 class CategoryServices {
   async getAll() {
-    const categories = await models.Category.findAll({ raw: true });
-    return categories;
+    const categories = await models.Category.findAll({
+      include: [
+        {
+          model: models.Product,
+          as: "products",
+        },
+      ],
+    });
+    const mappingCategories = categories.map((category) => {
+      const categoryData = category.get({ plain: true });
+      const { products, ...rest } = categoryData;
+      return {
+        ...rest,
+        totalProduct: products.length,
+      };
+    });
+    return mappingCategories;
   }
 }
 module.exports = new CategoryServices();
